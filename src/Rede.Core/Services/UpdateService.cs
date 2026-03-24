470782de713415438525aa1ae34b3e4f8d4bb863
caaatto <info@catto.at>
2026-03-24 15:25:08 +0000

    Bump version to 2.0.3-beta

--- src/Rede.Core/Services/UpdateService.cs
+++ src/Rede.Core/Services/UpdateService.cs
@@ -11,7 +11,7 @@ public class UpdateService
     private readonly string _branch;
 
     private const string GitHubRepo = "caaatto/rede";
-    private const string CurrentVersion = "2.0.2-beta";
+    private const string CurrentVersion = "2.0.3-beta";
 
     public event Action<string>? OnStatusUpdate;
     public event Action<string>? OnError;
